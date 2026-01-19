fec777b1a20353f2e30341cee05c806cc8600b58
Prakash9295 <ranabhatprakash00@gmail.com>
2026-01-19 22:57:21 +1100

    Fourth Commit

--- day4.py
+++ day4.py
@@ -9,7 +9,19 @@ print (a**b)
 print (a//b)
 print (a==b)
 print (a!=b)
-print (a>b)
-print (a<b)
-print (a>=b)
-print (a<=b)
+
+#write a program to check the relationship between two numbers
+if a>b:
+    print ("a is greater than b")
+elif a<b:
+    print ("a is less than b")
+else:
+    print ("a is equal to b")
+
+#write a program to check the relationship between two numbers
+if a>b:
+    print ("a is greater than b")
+elif a<b:
+    print ("a is less than b")
+else:
+    print ("a is equal to b")
